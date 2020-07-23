b06725b286d239208070128d6f848cd049f623d2
Gia Trung <giatrung1998pro@gmail.com>
2020-07-23 13:43:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/explicitlmtent_master/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.explicitlmtent_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+        String qString = extras.getString("qString");
+        final TextView textView = (TextView)
+                findViewById(R.id.textView1);
+        textView.setText(qString);
+    }
+    @Override
+    public void finish() {
+        Intent data = new Intent();
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        String returnString = editText1.getText().toString();
+        data.putExtra("returnData", returnString);
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
+    public void onClick(View view) {
+        finish();
+    }
+}
